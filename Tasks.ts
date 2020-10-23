d15dbb9a8643f3e506a2e7a63555b5f573a6add8
Sean Capaloff-Jones <seancjones@google.com>
2020-10-23 17:29:09 -0400

    snapshot saving from clasp when it sort of worked

--- Tasks.ts
+++ Tasks.ts
@@ -3,95 +3,89 @@
  */
 
 /**
- * Task API management library.
+ * Returns the results of listTasks by tasklist.
  */
-export class TaskApp {
-  /**
-   * Returns the results of listTasks by tasklist.
-   */
-  listTaskLists(): BucketedTasks {
-    const taskListBucket: BucketedTasks = {
-      pastDue: [],
-      dueSoon: [],
-      dueLater: [],
-      noDueDate: [],
-    };
+export function listTaskLists(): BucketedTasks {
+  const taskListBucket: BucketedTasks = {
+    pastDue: [],
+    dueSoon: [],
+    dueLater: [],
+    noDueDate: [],
+  };
 
-    const taskLists = Tasks.Tasklists.list();
-    if (taskLists.items) {
-      for (let i = 0; i < taskLists.items.length; i++) {
-        const taskList = taskLists.items[i];
-        const localBucket = this.listTasks(taskList.id);
-        taskListBucket.pastDue.concat(localBucket.pastDue);
-        taskListBucket.dueSoon.concat(localBucket.dueSoon);
-        taskListBucket.dueLater.concat(localBucket.dueLater);
-        taskListBucket.noDueDate.concat(localBucket.noDueDate);
-      }
+  const taskLists = Tasks.Tasklists.list();
+  if (taskLists.items) {
+    for (let i = 0; i < taskLists.items.length; i++) {
+      const taskList = taskLists.items[i];
+      const localBucket = this.listTasks(taskList.id);
+      taskListBucket.pastDue = taskListBucket.pastDue.concat(localBucket.pastDue);
+      taskListBucket.dueSoon = taskListBucket.dueSoon.concat(localBucket.dueSoon);
+      taskListBucket.dueLater = taskListBucket.dueLater.concat(localBucket.dueLater);
+      taskListBucket.noDueDate = taskListBucket.noDueDate.concat(localBucket.noDueDate);
     }
-    return taskListBucket;
   }
+  return taskListBucket;
+}
 
-  /**
-   * Returns a list of tasks split into buckets: pastDue, dueSoon, dueLater and noDueDate
-   */
-  listTasks(taskListId): BucketedTasks {
-    const tasks = Tasks.Tasks.list(taskListId);
-    const result: BucketedTasks = {
-      pastDue: [],
-      dueSoon: [],
-      dueLater: [],
-      noDueDate: []
-    };
-    if (tasks.items) {
-      for (let i = 0; i < tasks.items.length; i++) {
-        const task = tasks.items[i];
-        const now: number = new Date().getMilliseconds();
-        const taskDueDate: number = task.due ? new Date(task.due).getMilliseconds() : null;
-        if (taskDueDate === null) {
-          result["noDueDate"] = result["noDueDate"] || [];
-          result["noDueDate"].push(task);
-          continue;
-        }
-        if (taskDueDate - now <= 24*60*60*1000 && taskDueDate - now >= 0) {
-          result["dueSoon"] = result["dueSoon"] || [];
-          result["dueSoon"].push(task);
-          continue;
-        }
-        if (taskDueDate < now) {
-          result["pastDue"] = result["pastDue"] || [];
-          result["pastDue"].push(task);
-          continue;
-        }
-
-        result["dueLater"] = result["dueLater"] || [];
-        result["dueLater"].push(task);
+/**
+ * Returns a list of tasks split into buckets: pastDue, dueSoon, dueLater and noDueDate
+ */
+export function listTasks(taskListId: string): BucketedTasks {
+  const tasks = Tasks.Tasks.list(taskListId);
+  const result: BucketedTasks = {
+    pastDue: [],
+    dueSoon: [],
+    dueLater: [],
+    noDueDate: []
+  };
+  if (tasks.items) {
+    for (let i = 0; i < tasks.items.length; i++) {
+      const task = tasks.items[i];
+      const now: number = new Date().getMilliseconds();
+      const taskDueDate: number = task.due ? new Date(task.due).getMilliseconds() : null;
+      if (taskDueDate === null) {
+        result["noDueDate"] = result["noDueDate"] || [];
+        result["noDueDate"].push(task);
+        continue;
       }
+      if (taskDueDate - now <= 24 * 60 * 60 * 1000 && taskDueDate - now >= 0) {
+        result["dueSoon"] = result["dueSoon"] || [];
+        result["dueSoon"].push(task);
+        continue;
+      }
+      if (taskDueDate < now) {
+        result["pastDue"] = result["pastDue"] || [];
+        result["pastDue"].push(task);
+        continue;
+      }
+
+      result["dueLater"] = result["dueLater"] || [];
+      result["dueLater"].push(task);
     }
-    return result;
   }
+  return result;
+}
 
-  markTaskComplete(taskListId, taskId) {
+export function markTaskComplete(taskListId: string, taskId: string) {
 
-    const task = Tasks.Tasks.get(taskListId, taskId);
-      const request = {
-        "status": "completed",
-        "completed": new Date().toISOString(),
-      };
-    Tasks.Tasks.patch(request, taskListId, taskId);
-  }
+  const task = Tasks.Tasks.get(taskListId, taskId);
+  const request = {
+    "status": "completed",
+    "completed": new Date().toISOString(),
+  };
+  Tasks.Tasks.patch(request, taskListId, taskId);
+}
 
-  markTaskDelete(taskListId, taskId) {
+export function markTaskDelete(taskListId: string, taskId: string) {
+  const task = Tasks.Tasks.get(taskListId, taskId);
+  const request = {
+    "deleted": true,
+  };
+  Tasks.Tasks.patch(request, taskListId, taskId);
+}
 
-    const task = Tasks.Tasks.get(taskListId, taskId);
-      const request = {
-        "deleted": true,
-      };
-    Tasks.Tasks.patch(request, taskListId, taskId);
-  }
-
-  test() {
-    Logger.log(this.listTaskLists());
-  }
+export function test() {
+  Logger.log(this.listTaskLists());
 }
 
 export interface BucketedTasks {
